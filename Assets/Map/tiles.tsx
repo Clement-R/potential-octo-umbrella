2f67836235dc4a869305241ff8dc5de9cb2c97d5
C.Rondeau <rondeau.clement@gmail.com>
2017-05-25 21:40:51 +0200

    Added Tiled test map, also integrated CharacterController2D from Prime31

---
+++ Assets/Map/tiles.tsx
@@ -0,0 +1,373 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tiles" tilewidth="128" tileheight="128" tilecount="85" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/brick_grey.png"/>
+  <objectgroup draworder="index">
+   <object id="6" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/brick_red.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cactus_inside.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+   <object id="2" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cactus_side.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cactus_top.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cotton_blue.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cotton_green.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+   <object id="2" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cotton_red.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/cotton_tan.png"/>
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/dirt.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/dirt_grass.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/dirt_sand.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/dirt_snow.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/fence_stone.png"/>
+  <objectgroup draworder="index">
+   <object id="4" x="0" y="48" width="128" height="80"/>
+   <object id="5" x="0" y="30" width="24" height="18"/>
+   <object id="6" x="104" y="30" width="24" height="18"/>
+   <object id="7" x="139" y="-98" width="24" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/fence_wood.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/glass.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/glass_frame.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+   <object id="2" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass_brown.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass_tan.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass_top.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass2.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass3.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/grass4.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/gravel_dirt.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/gravel_stone.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/greysand.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/greystone.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/greystone_ruby.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/greystone_ruby_alt.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/greystone_sand.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/ice.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/lava.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/leaves.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/leaves_orange.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/leaves_orange_transparent.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/leaves_transparent.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/mushroom_brown.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/mushroom_red.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/mushroom_tan.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/oven.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/redsand.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/redstone.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/redstone_emerald.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/redstone_emerald_alt.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/redstone_sand.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/rock.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/rock_moss.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/sand.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/snow.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_browniron.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_browniron_alt.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_coal.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_coal_alt.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_diamond.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_diamond_alt.png"/>
+ </tile>
+ <tile id="57">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_dirt.png"/>
+ </tile>
+ <tile id="58">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_gold.png"/>
+ </tile>
+ <tile id="59">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_gold_alt.png"/>
+ </tile>
+ <tile id="60">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_grass.png"/>
+ </tile>
+ <tile id="61">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_iron.png"/>
+ </tile>
+ <tile id="62">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_iron_alt.png"/>
+ </tile>
+ <tile id="63">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_sand.png"/>
+ </tile>
+ <tile id="64">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_silver.png"/>
+ </tile>
+ <tile id="65">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_silver_alt.png"/>
+ </tile>
+ <tile id="66">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/stone_snow.png"/>
+ </tile>
+ <tile id="67">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/table.png"/>
+ </tile>
+ <tile id="68">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/track_corner.png"/>
+ </tile>
+ <tile id="69">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/track_corner_alt.png"/>
+ </tile>
+ <tile id="70">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/track_straight.png"/>
+ </tile>
+ <tile id="71">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/track_straight_alt.png"/>
+ </tile>
+ <tile id="72">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/trunk_bottom.png"/>
+ </tile>
+ <tile id="73">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/trunk_mid.png"/>
+ </tile>
+ <tile id="74">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/trunk_side.png"/>
+ </tile>
+ <tile id="75">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/trunk_top.png"/>
+ </tile>
+ <tile id="76">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/trunk_white_side.png"/>
+ </tile>
+ <tile id="77">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/trunk_white_top.png"/>
+ </tile>
+ <tile id="78">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/water.png"/>
+ </tile>
+ <tile id="79">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/wheat_stage1.png"/>
+ </tile>
+ <tile id="80">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/wheat_stage2.png"/>
+ </tile>
+ <tile id="81">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/wheat_stage3.png"/>
+ </tile>
+ <tile id="82">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/wheat_stage4.png"/>
+ </tile>
+ <tile id="83">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/wood.png"/>
+ </tile>
+ <tile id="84">
+  <image width="128" height="128" source="../../../_ASSETS/Kenney/voxel_pack/PNG/Tiles/wood_red.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+</tileset>
